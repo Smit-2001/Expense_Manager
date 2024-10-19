fed35d2e89dbf2c3805f4215488e9c74290a9d08
Smit <smitpshah63@gmail.com>
2024-10-19 16:32:05 +0530

    Implemented edit functionality

--- app.py
+++ app.py
@@ -28,6 +28,28 @@ def delete(id):
     db.session.commit()
     return redirect("/expenses")
 
+@app.route("/updateexpense/<int:id>")
+def updateexpense(id):
+    expense=Expense.query.filter_by(id=id).first()
+    return render_template("updateexpense.html",expense=expense)
+
+@app.route("/edit", methods=["POST"])
+def edit():
+    id=request.form["id"]
+    date=request.form["date"]
+    expensename=request.form["expensename"]
+    amount=request.form["amount"]
+    category=request.form["category"]
+
+    expense=Expense.query.filter_by(id=id).first()
+    expense.date=date
+    expense.expensename=expensename
+    expense.amount=amount
+    expense.category=category
+
+    db.session.commit()
+    return redirect("/expenses")
+
 @app.route('/expenses')
 def expenses():
     expenses=Expense.query.all()
